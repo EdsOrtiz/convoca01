456004acfdfb336df77698aaab271ae882a91921
Edsel Ortiz <ortiz_edsel@hotmail.com>
2024-12-06 10:54:34 -0600

    Save changes index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
     echo "Hi world!!!";
-    echo "\n Prueba de carga";
+    echo "Prueba de carga";
 
     echo "<img src='https://images.app.goo.gl/SViztJPZ4kqvXXBA8' width='200px' alt=''>";
