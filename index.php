9c778be430830d6a38b7253b385284c3ad7a60fd
Edsel Ortiz <ortiz_edsel@hotmail.com>
2024-12-06 10:49:30 -0600

    Add image Xalapa

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
     echo "Hi world!!!";
-    echo "Prueba de carga";
+    echo "\n Prueba de carga";
+
+    echo "<img src='https://images.app.goo.gl/SViztJPZ4kqvXXBA8' width='200px' alt=''>"
